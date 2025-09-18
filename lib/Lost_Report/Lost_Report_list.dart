c1676d89a3217c92288da4b75b272afa04a308c4
Deepa <deepakdev9567@gmail.com>
2025-09-18 10:23:17 +0530

    loast lead reporr

---
+++ lib/Lost_Report/Lost_Report_list.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class LostReportList extends StatelessWidget {
+  const LostReportList({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
